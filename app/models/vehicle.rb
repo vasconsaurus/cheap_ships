36e4af9d826f78f66cb90ce08061935d622a6ad1
manu vasconcelos <manu.vasconcelos@gmail.com>
2022-03-03 11:07:04 -0300

    add validations, work in progress

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -3,4 +3,8 @@ class Vehicle < ApplicationRecord
 
   validates :name, presence: true, uniqueness: true
   validates :category, presence: true, uniqueness: true
+  validates :price, presence: true, format: { with: /\d{1,3}(?:[.,]\d{3})*(?:[.,]\d{2})/ }
+  validates :description, presence: true, length: { minimum: 6 }
+  validates :kilometers, presence: true
+  validates :year, presence: true
 end
