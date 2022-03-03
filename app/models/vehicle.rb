e25ece6deb3c0ccc9862b217841fb99ed32ae5aa
manu vasconcelos <manu.vasconcelos@gmail.com>
2022-03-03 11:36:21 -0300

    price as float

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -1,10 +1,17 @@
 class Vehicle < ApplicationRecord
   belongs_to :user
 
-  validates :name, presence: true, uniqueness: true
-  validates :category, presence: true, uniqueness: true
-  validates :price, presence: true, format: { with: /\d{1,3}(?:[.,]\d{3})*(?:[.,]\d{2})/ }
-  validates :description, presence: true, length: { minimum: 6 }
-  validates :kilometers, presence: true
-  validates :year, presence: true
+  validates :name,        presence: true,
+                          uniqueness: true
+  validates :category,    presence: true,
+                          uniqueness: true
+  validates :price,       presence: true,
+                          numericality: true,
+                          format: { with: /\d{1,3}(?:[.,]\d{3})*(?:[.,]\d{2})/ }
+  validates :description, presence: true,
+                          length: { minimum: 6 }
+  validates :kilometer,  presence: true,
+                          numericality: true
+  validates :year,        presence: true,
+                          numericality: true
 end
