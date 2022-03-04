b0c6b3382e64bca5710aa613370c0354e1c968d3
Moises Marinho <moisesmarinhosilva87@gmail.com>
2022-03-04 11:40:03 -0300

    view order index

--- app/controllers/vehicles_controller.rb
+++ app/controllers/vehicles_controller.rb
@@ -1,6 +1,6 @@
 class VehiclesController < ApplicationController
   before_action :set_vehicle, only: %i[edit update show destroy]
-  skip_before_action :authenticate_user!, only: [:index, :show]
+  skip_before_action :authenticate_user!, only: %i[index show]
 
   def index
     @vehicles = Vehicle.all
