01baffabb9c0847aad43f4f41e467528b37fd01b
Felipe Brandalise <fabrandalise@gmail.com>
2022-03-04 11:12:47 -0300

    avatar implemented on sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,9 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   # before_action :configure_permitted_parameters, if: :devise_controller?
-  
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :vehicles, dependent: :destroy
+  has_one_attached :photo
 end
