ea8a06100d4b7bc8d9ddc7ef1f2d64f29927448a
manu vasconcelos <manu.vasconcelos@gmail.com>
2022-03-05 14:32:48 -0300

    wip

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,11 +2,19 @@ class OrdersController < ApplicationController
   def create
     @vehicle = Vehicle.find(params[:vehicle_id])
     @order = Order.new(vehicle: @vehicle, user: current_user)
+
+    authorize @order
+
     if @order.save
       @vehicle.update(available: false)
-      redirect_to vehicles_path, notice: "Your vehicle has been purchased"
+      redirect_to vehicle_order_path, notice: "Your vehicle has been purchased"
     else
-      redirect_to @vehicle, notice: "Failed"
+      redirect_to vehicle_path, notice: "Failed"
     end
+  end
+
+  def show
+    @order = Order.find(params[:id])
+    authorize @order
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :vehicles, dependent: :destroy
+  has_many :orders, dependent: :destroy
   has_one_attached :photo
 end
